8aca3b3b6cfb8870558e3e73faffa84a8f8510df
temi121 <58744726+temi121@users.noreply.github.com>
2022-07-17 23:47:26 -0400

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -17,28 +17,43 @@ var imgOrder = shuffle(["4", "9", "3", "2", "5", "8", "1", "6", "7"]);
 
 
 
-window.onload = function() {
+function fun1(){
     for (let r=0; r < rows; r++) {
-        for (let c=0; c < columns; c++) {
-
-            //<img id="0-0" src="img/1.jpg">
-            let tile = document.createElement("img");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.src = "img/" + imgOrder.shift() + ".jpg";
-
-            //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart);  //click an image to drag
-            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
-            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
-            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
-            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
-            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
-
-            document.getElementById("board").append(tile);
-
-        }
-    }
+                for (let c=0; c < columns; c++) {
+        
+                    //<img id="0-0" src="1.jpg">
+                    let tile = document.createElement("img");
+                    tile.id = r.toString() + "-" + c.toString();
+                    tile.src = imgOrder.shift() + ".jpg";
+        
+                    //DRAG FUNCTIONALITY
+                    tile.addEventListener("dragstart", dragStart);  //click an image to drag
+                    tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+                    tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
+                    tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
+                    tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+                    tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+        
+                    document.getElementById("board").append(tile);
+                }
+            }
 }
+
+function fun2(){
+    document.getElementById("my_audio").play();
+}
+
+
+var addFunctionOnWindowLoad = function(callback){
+      if(window.addEventListener){
+          window.addEventListener('load',callback,false);
+      }else{
+          window.attachEvent('onload',callback);
+      }
+}
+
+addFunctionOnWindowLoad(fun1);
+addFunctionOnWindowLoad(fun2);
 
 function dragStart() {
     currTile = this; //this refers to the img tile being dragged
